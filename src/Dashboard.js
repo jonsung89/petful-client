66ad2de18b1243515770e0fa2be216fb18a1b66b
jonsung89 <jonsung89@gmail.com>
2018-10-12 11:19:06 -0500

    added grid and styles

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,30 +7,31 @@ class Dashboard extends Component {
     const dog = this.props.dogToAdopt;
 
     return <div className="Dashboard">
-        <div>
+        <header className="Dash-header">
           <h1>Welcome to the Petful app</h1>
-        </div>
-        <section>
-          <header>
-            <h2>{cat.name}</h2>
-            <img src={cat.imageURL} alt={cat.imageDescription} />
-          </header>
-          <main>
-            <dl>
-              <dt>Age:</dt>
-              <dd>{cat.age}</dd>
-              <dt>Breed:</dt>
-              <dd>{cat.breed}</dd>
-              <dt>Sex:</dt>
-              <dd>{cat.sex}</dd>
-              <dt>Story:</dt>
-              <dd>{cat.story}</dd>
-            </dl>
-            <button>Adopt</button>
-          </main>
-        </section>
+        </header>
+        <div className="grid">
+          <section className="pet-info column">
+            <header>
+              <h2>{cat.name}</h2>
+              <img src={cat.imageURL} alt={cat.imageDescription} />
+            </header>
+            <main>
+              <dl>
+                <dt>Age:</dt>
+                <dd>{cat.age}</dd>
+                <dt>Breed:</dt>
+                <dd>{cat.breed}</dd>
+                <dt>Sex:</dt>
+                <dd>{cat.sex}</dd>
+                <dt>Story:</dt>
+                <dd>{cat.story}</dd>
+              </dl>
+              <button>Adopt</button>
+            </main>
+          </section>
 
-        <section>
+        <section className="pet-info column">
           <header>
             <h2>{dog.name}</h2>
             <img src={dog.imageURL} alt={dog.imageDescription} />
@@ -49,6 +50,9 @@ class Dashboard extends Component {
             <button>Adopt</button>
           </main>
         </section>
+
+        </div>
+
       </div>;
   }
 }
